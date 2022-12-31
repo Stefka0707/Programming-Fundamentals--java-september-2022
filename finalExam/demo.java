56438e3d39f026838f0efe7da058e402c8923ba8
Stefka Chill Out <114074774+Stefka0707@users.noreply.github.com>
2022-12-31 18:36:26 +0200

    Add files via upload

---
+++ finalExam/demo.java
@@ -0,0 +1,15 @@
+package finalExam;
+
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = 10;
+        double b =5.5;
+        a+=b;
+        System.out.println(a);
+
+    }
+}
